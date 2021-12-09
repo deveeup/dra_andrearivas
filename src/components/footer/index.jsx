77f94e7f56b0b2db3e66649c03e6e9ce9218eaac
David Rojas <jossedaviid1@gmail.com>
2021-12-08 20:06:17 -0500

    fix: change ws component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Whatsapp from '../../assets/img/whatsapp.png'
 import './index.scss';
 
 const year = new Date().getFullYear();
 
 const Footer = () => (
   <footer>
+    <a href="https://wa.me/573115423405" target="_blank" rel="noreferrer">
+      <img className="Contact-whatsapp" src={Whatsapp} alt="Contactenos a través de Whatsapp" />
+    </a>
     <p>
       © Dra. Andrea Rivas {year}
     </p>
