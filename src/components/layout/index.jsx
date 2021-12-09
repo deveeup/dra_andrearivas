50008efa2d163dd05830c992441a849b699a3795
David Rojas <jossedaviid1@gmail.com>
2021-12-08 20:01:36 -0500

    add: ws

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../header/index';
 import Footer from '../footer/index';
+import Whatsapp from '../../assets/img/whatsapp.png'
 import './index.scss';
 
 const Layout = (props) => (
   <div className="Layout">
     <Header />
       {props.children}
+      <a href="https://wa.me/573115423405" target="_blank" rel="noreferrer">
+        <img className="Layout-whatsapp" src={Whatsapp} alt="Contactenos a través de Whatsapp" />
+      </a>
     <Footer />
   </div>
 );
