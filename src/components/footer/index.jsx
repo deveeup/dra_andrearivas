5042a3ef938b82a6df2298fe3f6d0f8bb0495dd1
David Rojas <jossedaviid1@gmail.com>
2021-12-08 20:30:31 -0500

    add: phone contact

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Whatsapp from '../../assets/img/whatsapp.png'
+import Phone from '../../assets/img/phone.png'
 import './index.scss';
 
 const year = new Date().getFullYear();
 
 const Footer = () => (
   <footer>
-    <a href="https://wa.me/573115423405" target="_blank" rel="noreferrer">
-      <img className="Contact-whatsapp" src={Whatsapp} alt="Contactenos a través de Whatsapp" />
-    </a>
+    <div className="Contact">
+      <a href="tel:3115423405" target="_blank" rel="noreferrer">
+        <img className="Contact-phone" src={Phone} alt="Contactenos a través de una llamada" />
+      </a>
+      <a href="https://wa.me/573115423405" target="_blank" rel="noreferrer">
+        <img className="Contact-whatsapp" src={Whatsapp} alt="Contactenos a través de Whatsapp" />
+      </a>
+    </div>
     <p>
       © Dra. Andrea Rivas {year}
     </p>
