7648db0cf868716dc991166437a2cb0abcc3051c
David Rojas <jossedaviid1@gmail.com>
2021-12-08 19:31:34 -0500

    fix: add min height

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from '../header/index';
 import Footer from '../footer/index';
+import './index.scss';
 
 const Layout = (props) => (
-  <div>
+  <div className="Layout">
     <Header />
       {props.children}
     <Footer />
